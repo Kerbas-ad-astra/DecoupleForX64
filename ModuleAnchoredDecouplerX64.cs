b2d3d4400c3ccae0e9bc00a3438c63f1b097185f
Sarbian <Sarbian@sarbian.com>
2014-07-20 14:43:24 +0200

    Initial Commit
    ModuleDecoupleX64 works, but ModuleAnchoredDecouplerX64 does not apply
    the forces :(

---
+++ ModuleAnchoredDecouplerX64.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoupleForX64
+{
+    class ModuleAnchoredDecouplerX64 : ModuleAnchoredDecoupler
+    {
+        bool doForce = false;
+
+        [KSPAction("Decouple")]
+        public new void DecoupleAction(KSPActionParam param)
+        {
+            doForce = true;
+        }
+
+        [KSPEvent(guiName = "Decouple", guiActive = true)]
+        public new void Decouple()
+        {
+            doForce = true;
+        }
+
+        public new void OnDecouple()
+        {
+            doForce = true;
+        }
+
+        public override void OnActive()
+        {
+            if (base.staged)
+            {
+                doForce = true;
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (!this.isDecoupled && doForce)
+            {
+                print("[ModuleAnchoredDecouplerX64] ejectionForce " + this.ejectionForce);
+
+                this.ejectionForce = this.ejectionForce * 100;
+                base.OnDecouple();
+            }
+            doForce = false;
+        }
+    }
+}
---
+++ ModuleDecoupleX64.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace DecoupleForX64
+{
+    public class ModuleDecoupleX64 : ModuleDecouple
+    {
+        bool doForce = false;
+
+        [KSPAction("Decouple")]
+        public new void DecoupleAction(KSPActionParam param)
+        {
+            doForce = true;
+        }
+        [KSPEvent(guiName = "Decouple", guiActive = true)]
+        public new void Decouple()
+        {
+            doForce = true;
+        }
+
+        public new void OnDecouple()
+        {
+            doForce = true;
+        }
+
+        public override void OnActive()
+        {
+            if (base.staged)
+            {
+                doForce = true;
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (!this.isDecoupled && doForce)
+            {
+                print("[ModuleDecoupleX64] ejectionForce " + this.ejectionForce);
+                this.ejectionForce = this.ejectionForce * 100;
+                base.OnDecouple();
+            }
+            doForce = false;
+        }
+    }
+}
